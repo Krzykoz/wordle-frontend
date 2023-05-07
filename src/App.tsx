c6bc2e008417da1cdda011543facddf1e124f01f
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2023-05-07 19:55:46 +0200

    feat: send game if auth

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ function App() {
             Authorization: `Bearer ${userToken}`,
           },
         });
+        console.log({ response });
         if (!response.ok) {
           console.log('Something went wrong!');
         }
@@ -100,6 +101,7 @@ function App() {
               Authorization: `Bearer ${userToken}`,
             },
           });
+          console.log({ response });
           if (!response.ok) {
             console.log('Something went wrong!');
           }
